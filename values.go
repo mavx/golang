1ecd4a851e9093373e889bac6ee42db772709650
mavx <auyongminhao@gmail.com>
2017-05-28 03:48:57 +0800

    Day 1

---
+++ values.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1=", 2)
+	fmt.Println("7/3=", 7/3)
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+}
